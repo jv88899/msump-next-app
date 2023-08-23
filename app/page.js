1de14c7a296c36d495cd3c134e24adb2cfc71538
jv88899 <jv88899@gmail.com>
2023-08-23 17:25:50 -0500

    set up basic mobile landing page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main>
-      <button className="btn-primary">Hey</button>
-    </main>
+    <div className="max-w-[1280px] mx-auto h-screen px-4 pt-4">
+      <h1 className="text-center mt-4">My Still Untitled Music Project</h1>
+      <div className="mt-80">
+        <button className="btn-primary w-72 h-12 text-xl mx-auto mb-2 block text-center">
+          Sign up
+        </button>
+        <button className="btn-outline w-72 h-12 text-xl mx-auto block text-center">
+          Learn More
+        </button>
+      </div>
+    </div>
   );
 }
