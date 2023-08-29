f6436859ce0f5fda6261a923be18c847e831c841
jv88899 <jv88899@gmail.com>
2023-08-28 19:35:17 -0500

    set up redirects to dashboard if a user is logged in

---
+++ app/dashboard/page.js
@@ -0,0 +1,22 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  if (!data?.session) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      {data?.session ? (
+        <p>Hello, {data.session.user.email}</p>
+      ) : (
+        <p>No active user</p>
+      )}
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  if (data.session) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="max-w-[1280px] mx-auto px-4 pt-4">
       <header className="p-2">
