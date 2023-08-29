0db72ceff315175131cc7dee4f2062954f43c492
jv88899 <jv88899@gmail.com>
2023-08-28 20:07:34 -0500

    added ability to sign in

--- app/(auth)/signin/page.js
+++ app/(auth)/signin/page.js
@@ -1,12 +1,32 @@
 "use client";
 
 import AuthForm from "@/app/(auth)/AuthForm";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function SignIn() {
+  const router = useRouter();
+  const [error, setError] = React.useState("");
+
   async function handleSubmit(e, email, password) {
     e.preventDefault();
-    console.log("email/password", email, password);
+    setError("");
+
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    }
+
+    if (!error) {
+      router.push("/dashboard");
+    }
   }
 
   return (
@@ -20,6 +40,7 @@ export default function SignIn() {
             Sign up.
           </Link>
         </p>
+        {error ? <p>{error}</p> : null}
       </div>
       <Link href="/" className="w-full text-center block mt-64">
         Home
